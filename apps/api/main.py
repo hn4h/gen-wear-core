da15e36e29e0f05d82227fb75c7f1cae1e167823
Hnah <hanh113004@gmail.com>
2026-02-02 00:37:06 +0700

    feat: Introduce core FastAPI application, update API URL configuration in docker-compose, and refine gitignore.

--- apps/api/main.py
+++ apps/api/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Gen Wear API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[ "https://genwear.io.vn","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
